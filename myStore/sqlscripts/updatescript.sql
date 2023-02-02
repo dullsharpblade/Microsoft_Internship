65f6cfac4e3cfe690c74aaf0ad6219d82546cf87
dulls <dullsharpblade@gmail.com>
2023-02-02 11:39:56 -0800

    committing sql scripts

---
+++ myStore/sqlscripts/updatescript.sql
@@ -0,0 +1,11 @@
+ update customer set customerid = 2 where name = 'Jane Doe' 
+  SELECT *into customer1 from customer
+  truncate table customer
+  insert into customer ([name]
+      ,[address]
+      ,[state]
+      ,[zipcode])
+	  select [name]
+      ,[address]
+      ,[state]
+      ,[zipcode] from customer1
