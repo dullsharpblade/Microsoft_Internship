0764063d2e9258f2ef17e593d6e2e35babac743d
dulls <dullsharpblade@gmail.com>
2023-02-09 14:54:06 -0800

    Checkin

--- myStore/myStoreDatabase/customer.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[customer](
-	[customerid] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](100) NULL,
-	[address] [varchar](100) NULL,
-	[state] [varchar](2) NULL,
-	[zipcode] [varchar](5) NULL,
-	[phonenumber] [varchar](20) NULL, 
-    [country] VARCHAR(30) NULL
-) ON [PRIMARY]
---
+++ myStore/myStoreDatabase/scripts/insertscript.sql
@@ -0,0 +1,21 @@
+USE [myStore]
+GO
+
+/*INSERT INTO [dbo].[customer]
+           ([name]
+           ,[address]
+           ,[state]
+           ,[zipcode])
+     VALUES
+           ('John Doe'
+           ,'2364 SE Birch'
+           ,'CA'
+           ,'20638')
+		   
+GO
+DELETE customer where customerid = 5
+
+Alter table customer add phonenumber varchar (10) 
+update customer set phonenumber = '265-624-3545' where customerid = 6
+Alter table customer alter column phonenumber varchar (20)
+/*
