caca557242de6c3622bcf5c09794e240a8935e70
dulls <dullsharpblade@gmail.com>
2023-02-16 11:37:37 -0800

    checking in product table

--- myStore/myStoreDatabase/tables/product.sql
+++ myStore/myStoreDatabase/tables/product.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[product]
 (
-	[productid] INT NOT NULL PRIMARY KEY, 
+	[productid] INT IDENTITY NOT NULL PRIMARY KEY, 
     [product] VARCHAR(20) NULL, 
     [description] VARCHAR(200) NULL, 
     [price] VARCHAR(10) NULL
